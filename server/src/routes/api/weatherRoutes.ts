7744243de5d4d464070a28f1edda1f9bf14fc6c0
Bbbjrn <bjorntimothyjohansson@gmail.com>
2024-08-06 11:22:37 -0600

    revert to original pages

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -3,26 +3,17 @@ const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
-import { getWeather, getHistory, addCity, removeCity } from '../../service/weatherService.js';
+
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', async (req: Request, res: Response) => {
   // TODO: GET weather data from city name
-  const { city } = req.body;
-  try {
-    const weatherData = await getWeather(city);
-    res.json(weatherData);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve weather' });
-  }
+
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {
-  const history = await getHistory();
-  res.json(history);
-});
+router.get('/history', async (req: Request, res: Response) => {});
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req: Request, res: Response) => {});
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'; 
-dotenv.config();
+dotenv.config(); 
 
 // TODO: Define an interface for the Coordinates object
 

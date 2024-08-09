677c908a57fde025e2e0a6ed8a68ae5ba74c3233
Bbbjrn <bjorntimothyjohansson@gmail.com>
2024-08-09 11:13:39 -0600

    update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'path'; // Import path module for handling file paths
 dotenv.config();
 
 // Import the routes
-import routes from './routes/index';
+import routes from './routes/index.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// Serve static files from the entire client dist folder
-app.use(express.static(path.join(__dirname, '../../client/dist')));
-
-// Implement middleware for parsing JSON and urlencoded form data
+// Serves static files in the entire client's dist folder
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Implement middleware to connect the routes
 app.use(routes);
-
-// Catch-all route to serve index.html for any client-side routes
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-});
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
-

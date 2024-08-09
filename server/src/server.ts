95bab62ef91e6bfab354574a1162d7dd38f90a71
Bbbjrn <bjorntimothyjohansson@gmail.com>
2024-08-09 11:08:30 -0600

    update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,30 @@
-import express from 'express';
-import path from 'path';
 import dotenv from 'dotenv';
-import weatherRoutes from './routes/api/weatherRoutes.js';
-import htmlRoutes from './routes/htmlRoutes.js';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import express from 'express';
+import path from 'path'; // Import path module for handling file paths
 dotenv.config();
 
-import routes from './routes/index.js';
+// Import the routes
+import routes from './routes/index';
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
+// Serve static files from the entire client dist folder
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+// Implement middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
-app.use(express.static(path.join(__dirname, '../client/dist')));
-// TODO: Implement middleware to connect the routes
-app.use('/api/weather', weatherRoutes);
-app.use('/', htmlRoutes);
-
+// Implement middleware to connect the routes
 app.use(routes);
+
+// Catch-all route to serve index.html for any client-side routes
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
